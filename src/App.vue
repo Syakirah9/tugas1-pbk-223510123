45152c5cff2059b6604fdce36af838bf24cdf9b9
Syakirah9 <syakirahqaitah9@gmail.com>
2024-04-04 11:58:18 +0700

    tugas1

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/img.jpeg" width="230" height="28npm run d0" />
+
+    <div class="wrapper">
+      <HelloWorld msg="Syakirah Qanitah" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+  border-radius: 75%;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,75 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Tujuan</template>
+    Tujuan tugas ini dibuat untuk memenuhi tugas Praktikum Pemrograman Berbasis Komponen atau disebut dengan PBK.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Profil</template>
+    <table id="data">
+      <tr>
+        <th>Nama Lengkap</th>
+        <th>Nama Panggilan</th>
+        <th>Tempat, Tanggal Lahir</th>
+        <th>Pendidikan</th>
+      </tr>
+
+      <tr>
+        <td>Syakirah Qanitah</td>
+        <td>Syakirah</td>
+        <td>Pekanbaru, 09 September 2004</td>
+        <td>Universitas Islam Riau (UIR)</td>
+      </tr>
+    </table>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Data Hobi</template>
+    Saya mempunyai hobi seperti sebagai
+    berikut ini :<br />
+    <ul>
+      <li>Bermain Basket</li>
+      <li>Membaca Buku</li>
+      <li>Nonton NBA</li>
+    </ul>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Warna Kesukaan :</template>
+    <ol>
+      <li>Putih</li>
+      <li>Hitam</li>
+      <li>Merah</li>
+      <li>Pink</li>
+    </ol>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Hubungi</template>
+    WA : 08-123-....<br />
+  </WelcomeItem>
+</template>
